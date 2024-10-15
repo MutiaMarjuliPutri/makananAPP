a2fbbed747cbc8280d8e57454efc9fe72c5d7c88
MutiaMarjuliPutri <162146690+MutiaMarjuliPutri@users.noreply.github.com>
2024-10-15 21:33:55 +0700

    Initial commit

---
+++ app/src/main/java/com/mutia/foodapps/ListOrderActivity.kt
@@ -0,0 +1,40 @@
+package com.mutia.foodapps
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mutia.foodapps.adapter.OrderAdapter
+import com.mutia.foodapps.model.ModelListOrder
+
+class ListOrderActivity : AppCompatActivity() {
+    private lateinit var RecyclerViewMakanan : RecyclerView
+    private lateinit var AdapterMakanan : OrderAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_order)
+
+        val menuMakanan = listOf(
+            ModelListOrder(
+                R.drawable.chickencurry,
+                "Chicken Curry",
+                "29 nov, 01.09 pm",
+                "$50.00",
+                "2 Items"
+            ),
+            ModelListOrder(R.drawable.burger, "Bean burger", "10 mei, 06.10 pm", "$50.00", "2 Items"),
+            ModelListOrder(R.drawable.kopi, "Coffe latte", "06 nov, 02.09 pm", "$50.00", "2 Items"),
+            ModelListOrder(R.drawable.chickencurry, "Chicken Curry", "07 nov, 07.45 pm", "$50.00", "2 Items"),
+            ModelListOrder(R.drawable.starberi, "Strawberry", "24 june, 07.00 pm", "$50.00", "2 Items")
+
+        )
+
+
+        RecyclerViewMakanan = findViewById(R.id.RvMakanan)
+        RecyclerViewMakanan.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        AdapterMakanan = OrderAdapter(menuMakanan)
+        RecyclerViewMakanan.adapter = AdapterMakanan
+    }
+}
